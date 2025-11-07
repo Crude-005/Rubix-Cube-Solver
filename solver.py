e3c53d2f26aa97a872efaee638c76ffa39c437ca
Crude-005 <chiragsugla@gmail.com>
2025-11-07 17:03:38 +0530

    draft 2.0 changed 3d list to numpy array

--- solver.py
+++ solver.py
@@ -1,5 +1,7 @@
 import random
 import queue
+import numpy as np
+import time
 
 
 N = 3
@@ -82,12 +84,9 @@ def showCube(cube):
 
 
 def getFinalState(n):
-    finalState = list()
+    finalState = np.arange(6 * n * n, dtype=np.int64).reshape(6, n, n)
     for i in range(1, 7):
-        face = list()
-        for _ in range(n):
-            face.append([i] * n)
-        finalState.append(face)
+        finalState[i - 1][::] = i
     return finalState
 
 
@@ -104,10 +103,10 @@ def initialiseCube(n):
     select = input(
         "Enter 0 to use default Cube or 1 to input Custom Cube or 2 for random Cube : "
     )
-    startState = list()
     if select == "2":
         validMoves = generateValidMoves(n)
-        moves = [random.choice(validMoves) for _ in range(3)]
+        movesToRandomise = 6
+        moves = [random.choice(validMoves) for _ in range(movesToRandomise)]
         print("Moves")
         print(*moves, sep="\n")
         print()
@@ -117,6 +116,7 @@ def initialiseCube(n):
         return startState
 
     elif select == "1":
+        startState = list()
         face = list()
         print("Front Face:\n ")
         for i in range(n):
@@ -147,6 +147,7 @@ def initialiseCube(n):
         for i in range(n):
             face.append(list(map(int, input(f"Enter layer {i+1}: ").split())))
         startState.append(face)
+        # print(startState)
     else:
         if n == 3:
             startState = [
@@ -162,33 +163,34 @@ def initialiseCube(n):
             startState = getFinalState(n)
 
         # =====================================
-    return startState
+    return np.array(startState)
 
 
-def faceRotate(face, val, clockwise=True):
-    def rotate_right(f):
-        return [list(reversed(col)) for col in zip(*f)]
+# def faceRotate(face, val, clockwise=True):
+#     print(face)
 
-    def rotate_left(f):
-        return [list(col) for col in zip(*f)][::-1]
+#     def rotate_right(f):
+#         return np.array([np.array(reversed(col)) for col in zip(*f)])
 
-    result = [row[:] for row in face]
-    if clockwise:
-        for _ in range(val):
-            result = rotate_right(result)
-    else:
-        for _ in range(val):
-            result = rotate_left(result)
-    return result
+#     def rotate_left(f):
+#         return np.flip(np.array(np.array(col) for col in zip(*f)))
+
+#     if clockwise:
+#         for _ in range(val):
+#             face = rotate_right(face)
+#     else:
+#         for _ in range(val):
+#             face = rotate_left(face)
+#     return face
 
 
 def playMove(move, previousState):
-    currentState = previousState.copy()
+    currentState = np.copy(previousState)
     n = len(currentState[0])
 
     if move[0] == 1:
         if move[1] == 1:
-            currentState[0] = faceRotate(currentState[0], move[2])
+            currentState[0] = np.rot90(currentState[0], k=-move[2])
         for _ in range(move[2]):
             for i in range(n):
                 (
@@ -204,7 +206,7 @@ def playMove(move, previousState):
                 )
     elif move[0] == 6:
         if move[1] == 1:
-            currentState[5] = faceRotate(currentState[5], move[2], False)
+            currentState[5] = np.rot90(currentState[5], k=move[2])
         for _ in range(move[2]):
             for i in range(n):
                 (
@@ -220,37 +222,39 @@ def playMove(move, previousState):
                 )
     elif move[0] == 2:
         if move[1] == 1:
-            currentState[1] = faceRotate(currentState[1], move[2])
+            currentState[1] = np.rot90(currentState[1], k=-move[2])
         for _ in range(move[2]):
-            (
-                currentState[5][move[1] - 1],
-                currentState[2][n - move[1]],
-                currentState[0][n - move[1]],
-                currentState[3][n - move[1]],
-            ) = (
-                currentState[3][n - move[1]][::-1],
-                currentState[5][move[1] - 1][::-1],
-                currentState[2][n - move[1]],
-                currentState[0][n - move[1]],
-            )
+            for i in range(n):
+                (
+                    currentState[3][n - move[1]][i],
+                    currentState[0][n - move[1]][i],
+                    currentState[2][n - move[1]][i],
+                    currentState[5][move[1] - 1][n - i - 1],
+                ) = (
+                    currentState[0][n - move[1]][i],
+                    currentState[2][n - move[1]][i],
+                    currentState[5][move[1] - 1][n - i - 1],
+                    currentState[3][n - move[1]][i],
+                )
     elif move[0] == 5:
         if move[1] == 1:
-            currentState[4] = faceRotate(currentState[4], move[2], False)
+            currentState[4] = np.rot90(currentState[4], k=move[2])
         for _ in range(move[2]):
-            (
-                currentState[5][n - move[1]],
-                currentState[2][move[1] - 1],
-                currentState[0][move[1] - 1],
-                currentState[3][move[1] - 1],
-            ) = (
-                currentState[3][move[1] - 1][::-1],
-                currentState[5][n - move[1]][::-1],
-                currentState[2][move[1] - 1],
-                currentState[0][move[1] - 1],
-            )
+            for i in range(n):
+                (
+                    currentState[5][n - move[1]][n - i - 1],
+                    currentState[2][move[1] - 1][i],
+                    currentState[0][move[1] - 1][i],
+                    currentState[3][move[1] - 1][i],
+                ) = (
+                    currentState[3][move[1] - 1][i],
+                    currentState[5][n - move[1]][n - i - 1],
+                    currentState[2][move[1] - 1][i],
+                    currentState[0][move[1] - 1][i],
+                )
     elif move[0] == 4:
         if move[1] == 1:
-            currentState[3] = faceRotate(currentState[3], move[2])
+            currentState[3] = np.rot90(currentState[3], k=-move[2])
         for _ in range(move[2]):
             for i in range(n):
                 (
@@ -266,7 +270,7 @@ def playMove(move, previousState):
                 )
     elif move[0] == 3:
         if move[1] == 1:
-            currentState[2] = faceRotate(currentState[2], move[2], False)
+            currentState[2] = np.rot90(currentState[2], k=move[2])
         for _ in range(move[2]):
             for i in range(n):
                 (
@@ -297,54 +301,63 @@ def cubeToString(cube):
 
 
 def stringToCube(s):
-    n = (s / 6) ** 1 / 2
+    if s == "":
+        return None
+    n = int((len(s) / 6) ** (1 / 2))
+
     cnt = 0
     cube = list()
-    for i in 6:
+    for i in range(6):
         face = list()
         for j in range(n):
             layer = list()
             for k in range(n):
-                layer.append(s[int(cnt)])
+                layer.append(int(s[cnt]))
                 cnt += 1
-        face.append(layer)
-    cube.append(face)
-    return cube
+            face.append(layer)
+        cube.append(face)
+    return np.array(cube)
 
 
-def solve(startState, finalState, n):
+def solve(startState: np.ndarray, finalState, n):
 
     validMoves = generateValidMoves(n)
     parent = dict()
-    parent[cubeToString(startState)] = [None, None]
+    showCube(startState)
+
+    finalStateString = cubeToString(finalState)
+    parent[cubeToString(startState)] = [None, ""]
     q = queue.Queue()
     q.put(startState)
-    solutionFound = False
+    solutionNotFound = True
 
-    while not q.empty() or solutionFound:
+    while not q.empty() and solutionNotFound:
         currentState = q.get()
+        currentStateString = cubeToString(currentState)
         for move in validMoves:
+            # showCube(currentState)
+            # print(move)
             nextState = playMove(move, currentState)
+            # showCube(nextState)
             if parent.get(cubeToString(nextState), -1) == -1:
-                parent[cubeToString(nextState)] = (move, currentState)
+                parent[cubeToString(nextState)] = (move, currentStateString)
                 q.put(nextState)
             else:
                 continue
-            if nextState == finalState:
-                solutionFound = True
+            if cubeToString(nextState) == finalStateString:
+                solutionNotFound = False
                 break
-    if not solutionFound:
+    if solutionNotFound:
         print("Sorry buddy you messed up")
     else:
         backtrackArr = list()
         solutionMoves = list()
-        backtrackArr.append(finalState)
-        curr = finalState
-        while curr:
-            st = cubeToString(curr)
-            solutionMoves.append(parent[st][0])
-            backtrackArr.append(parent[st][1])
-            curr = parent[st][1]
+        backtrackArr.append(finalStateString)
+        curr = finalStateString
+        while curr != "":
+            solutionMoves.append(parent[curr][0])
+            backtrackArr.append(stringToCube(parent[curr][1]))
+            curr = parent[curr][1]
         print(*backtrackArr[::-1], sep="\n\n")
         print(solutionMoves[::-1])
 
@@ -352,8 +365,11 @@ def solve(startState, finalState, n):
 def main():
     finalState = getFinalState(N)
     startState = initialiseCube(N)
+    start = time.time()
 
     solve(startState, finalState, N)
+    end = time.time()
+    print(f"Time Taken = {end - start:.3f} s.")
 
 
 if __name__ == "__main__":
